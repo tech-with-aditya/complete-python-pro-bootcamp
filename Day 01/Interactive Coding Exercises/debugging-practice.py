a7f376ebf3a7e286ceec72177fa68bb714ea33cd
tech-with-aditya <112870829+tech-with-aditya@users.noreply.github.com>
2022-09-18 20:31:09 +0530

    Update debugging-practice.py

--- Day 01/Interactive Coding Exercises/debugging-practice.py
+++ Day 01/Interactive Coding Exercises/debugging-practice.py
@@ -1,4 +1,4 @@
-#Fix the code below 👇
+# Fix the code below 👇
 
 print(Day 1 - String Manipulation")
 print("String Concatenation is done with the "+" sign.")
@@ -6,13 +6,13 @@ print("String Concatenation is done with the "+" sign.")
 print(("New lines can be created with a backslash and n.")
 
 
-#Fixed code
+# Fixed code
 
-#missing quotation mark after the opening parenthesis.
+# Missing quotation mark after the opening parenthesis.
 print("Day 1 - String Manipulation")
-#outer double quotes changed to single quotes.
+# Outer double quotes changed to single quotes.
 print('String Concatenation is done with the "+" sign.')
-#extra identation removed.
+# Extra identation removed.
 print('e.g. print("Hello " + "world")')
-#extra opening paranthesis in print function removed.
+# Extra opening paranthesis removed.
 print("New lines can be created with a backslash and n.")
