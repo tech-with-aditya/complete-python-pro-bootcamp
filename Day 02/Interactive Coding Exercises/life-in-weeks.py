49cd8a5285cb2124b1af4251fcba227e404ab9ef
tech-with-aditya <112870829+tech-with-aditya@users.noreply.github.com>
2022-09-19 23:19:39 +0530

    Create life-in-weeks.py

---
+++ Day 02/Interactive Coding Exercises/life-in-weeks.py
@@ -0,0 +1,6 @@
+age = input("Enter your current age: ")
+year = 90 - int(age)
+months = year * 12
+weeks = year * 52
+days = year * 365
+print(f"You have {days} days, {weeks} weeks, and {months} months left.")
