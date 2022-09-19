211861e05ca0ad2e1f27de3f63f5d379111419f5
tech-with-aditya <112870829+tech-with-aditya@users.noreply.github.com>
2022-09-19 23:16:47 +0530

    Update main.py

--- Day 02/main.py
+++ Day 02/main.py
@@ -1,32 +1,38 @@
 print(8 / 3)
 print(int(8 / 3))
 
-#round operation
+# round operation
 print(round(8 / 3, 2))
 print(round(2.66666666, 2))
 
-#floor operation
+# floor operation
 print(8 // 3)
 
-#type of below expression will be float
+# Type of below expression will be float
 print(type(8 / 3))
-#type of below expression will be int
+
+# Type of below expression will be int
 print(type(8 // 3))
 
 score = 0
-#shorthand expression for score = score + 1
+
+# Shorthand expression for score = score + 1
 score += 1
-#shorthand expression for score = score - 1
+
+# Shorthand expression for score = score - 1
 score -= 1
 
-#int datatype
+# int datatype
 score = 2
-#float datatype
+
+# float datatype
 height = 1.68
-#boolean datatype
+
+# boolean datatype
 isWinning = True
 
-#normal print expression
+# Normal print expression
 print("Your score is " + score + ", your height is " + height + ", you are winning is " + isWinning)
-#F-string
+
+# f-string
 print(f"Your score is {score}, your height is {height}, you are winning is {isWinning}")
