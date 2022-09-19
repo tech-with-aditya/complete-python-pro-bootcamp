b624ddea8781c9b1ba0acfc03b29f79672f81aae
tech-with-aditya <112870829+tech-with-aditya@users.noreply.github.com>
2022-09-19 23:34:10 +0530

    Update main.py

--- Day 03/main.py
+++ Day 03/main.py
@@ -5,10 +5,10 @@ if height >= 120:
   print("You can ride the rollercoaster.")
   age = int(input("Enter your age: "))
   if age < 12:
-    print("You need to pay ₹600/-")
+    print("You need to pay ₹400/-")
   elif age <= 18:
-    print("You need to pay ₹800/-")
+    print("You need to pay ₹600/-")
   else:
-    print("You need to pay ₹1200/-")
+    print("You need to pay ₹800/-")
 else:
   print("Sorry! The minimum height required to ride the rollercoaster is 120cm.")
