b04e3eef9cb171d080a27970cb43a37e3ec34d62
tech-with-aditya <112870829+tech-with-aditya@users.noreply.github.com>
2022-09-21 22:39:01 +0530

    Create lists.py

---
+++ Day 04/Lists/lists.py
@@ -0,0 +1,14 @@
+state1 = "Delhi"
+state2 = "Bengaluru"
+state3 = "Chennai"
+state4 = "Mumbai"
+
+states_of_India = ["Delhi", "Bengaluru", "Chennai", "Mumbai"]
+
+states_of_India[1] = "Bangalore"
+
+states_of_India.append("Dehradun")
+
+states_of_India.extend(["Jaipur", "Guwahati", "Hyderabad"])
+
+print(states_of_India)
