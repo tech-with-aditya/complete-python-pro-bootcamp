f0773b6fdf2663f7a2918e64b1a5903693cb84e7
tech-with-aditya <112870829+tech-with-aditya@users.noreply.github.com>
2022-09-21 22:44:48 +0530

    Create main.py

---
+++ Day 05/main.py
@@ -0,0 +1,12 @@
+fruits = ["Apple", "Peach", "Pear"]
+for fruit in fruits:
+    print(fruit)
+    print(fruit + " Pie")
+    print(fruits)
+
+# for n in range(1, 11, 2):
+#     print(n)
+sum = 0
+for n in range(1, 101):
+    sum += n
+print(sum)
