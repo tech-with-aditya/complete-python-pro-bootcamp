bb7097920ae660e07a2e1b175a96929bcf4d1ae6
tech-with-aditya <112870829+tech-with-aditya@users.noreply.github.com>
2022-09-21 22:42:44 +0530

    Update treasure-map.py

--- Day 04/Interactive Coding Exercises/treasure-map.py
+++ Day 04/Interactive Coding Exercises/treasure-map.py
@@ -7,13 +7,13 @@ print(f"{row1}\n{row2}\n{row3}")
 position = input("Where do you want to put the treasure? ")
 # 🚨 Don't change the code above 👆
 
-#Write your code below this row 👇
+# Write your code below this row 👇
 
 col = int(position[0])
 row = int(position[1])
 map[row - 1][col - 1] = "X"
 
-#Write your code above this row 👆
+# Write your code above this row 👆
 
 # 🚨 Don't change the code below 👇
 print(f"{row1}\n{row2}\n{row3}")
